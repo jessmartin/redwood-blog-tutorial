b591ca032e755e0f389991ef0756172297a33f0c
Jess Martin <jessmartin@gmail.com>
2020-03-19 11:53:50 -0400

    Adding home page

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+      <p>Magic refresh! ✨</p>
+    </div>
+  )
+}
+
+export default HomePage
